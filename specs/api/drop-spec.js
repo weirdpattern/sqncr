8588ed6c00bed898b2f01c2d56bcf75b9741c42f
weirdpattern <patricio@weirdpattern.com>
2016-11-01 12:13:58 -0500

    Add iterators for maps and sets Add drop specs Add base specs
    changelog
    sqncr/
      lib/
        utils/
          getIterator.js
            - Add map iterator
            - Add set iterator
      specs/
        api/
          Add drop-spec.js
          Add base.js
          Remove sequence-spec.js

--- specs/api/drop-spec.js
+++ specs/api/drop-spec.js
@@ -1,18 +1,69 @@
+import execute from '../base';
 import drop from '../../lib/api/drop';
-import Sequence from '../../lib/sequence';
 
-describe('drop', () => {
-  describe('standalone', () => {
-    it('works with arrays', () => {
-      const result = drop([1, 2, 3, 4], 2).toArray();
-      expect(result).toEqual([3, 4]);
-    });
-  });
-
-  describe('sequence api', () => {
-    it('works with arrays', () => {
-      const result = new Sequence([1, 2, 3, 4]).drop(2).toArray();
-      expect(result).toEqual([3, 4]);
+describe('api', () => {
+  describe('drop', () => {
+    describe('standalone', () => {
+      execute({
+        'returns the same input when using drop count lesser than 1': {
+          input: [
+            drop('unexpected', -2).toArray().join(''),
+            drop('unexpected', -1).toArray().join(''),
+            drop('unexpected', 0).toArray().join(''),
+          ],
+          output: 'unexpected',
+        },
+        'returns the empty when the count is greater than the length of the input': {
+          input: [
+            drop('unexpected', 100).toArray().join(''),
+            drop('unexpected', 200).toArray().join(''),
+            drop('unexpected', 300).toArray().join(''),
+          ],
+          output: '',
+        },
+        'works with strings': {
+          input: drop('unexpected', 2).toArray().join(''),
+          output: 'expected',
+        },
+        'works with arrays': {
+          criteria: 'toEqual',
+          input: [drop([1, 2, 3, 4], 2).toArray()],
+          output: {
+            value: [3, 4],
+          },
+        },
+        'works with maps': {
+          criteria: 'toEqual',
+          input: [drop(new Map([[1, 1], [2, 2], [3, 3], [4, 4]]), 2).toArray()],
+          output: {
+            value: [3, 4],
+          },
+        },
+        'works with sets': {
+          criteria: 'toEqual',
+          input: [drop(new Set([1, 2, 3, 4]), 2).toArray()],
+          output: {
+            value: [3, 4],
+          },
+        },
+        'works with typed arrays': {
+          criteria: 'toEqual',
+          input: [
+            drop(new Float32Array([1, 2, 3, 4]), 2).toArray(),
+            drop(new Float64Array([1, 2, 3, 4]), 2).toArray(),
+            drop(new Int8Array([1, 2, 3, 4]), 2).toArray(),
+            drop(new Int16Array([1, 2, 3, 4]), 2).toArray(),
+            drop(new Int32Array([1, 2, 3, 4]), 2).toArray(),
+            drop(new Uint8Array([1, 2, 3, 4]), 2).toArray(),
+            drop(new Uint8ClampedArray([1, 2, 3, 4]), 2).toArray(),
+            drop(new Uint16Array([1, 2, 3, 4]), 2).toArray(),
+            drop(new Uint32Array([1, 2, 3, 4]), 2).toArray(),
+          ],
+          output: {
+            value: [3, 4],
+          },
+        },
+      });
     });
   });
 });
---
+++ specs/base.js
@@ -0,0 +1,50 @@
+function isPrimitive(candidate : any) {
+  return candidate == null || (typeof candidate !== 'object' && typeof candidate !== 'function');
+}
+
+function process(input, output, criteria) {
+  let negated;
+  let criterion;
+  let expectation;
+
+  const outputs = Array.isArray(output) ? output : [output];
+  outputs.forEach((descriptor) => {
+    expectation = expect(input);
+    criterion = descriptor.criteria || criteria || 'toBe';
+    negated = descriptor.negated || false;
+
+    if (typeof descriptor === 'object') {
+      if (negated) {
+        expectation = expectation.not;
+      }
+    }
+
+    if (isPrimitive(descriptor)) {
+      descriptor = {
+        value: descriptor,
+      };
+    }
+
+    expectation[criterion](
+      typeof descriptor === 'function' ? descriptor(input) : descriptor.value
+    );
+  });
+}
+
+export default function execute(tests) {
+  Object.keys(tests).forEach((key) => {
+    const description = key;
+    const descriptor = tests[key];
+
+    it(description, () => {
+      const inputs = Array.isArray(descriptor.input) ? descriptor.input : [descriptor.input];
+      inputs.forEach((input) => {
+        if (descriptor.log) {
+          console.log(`input: ${input}`);              // eslint-disable-line no-console
+          console.log(`output: ${descriptor.output}`); // eslint-disable-line no-console
+        }
+        process(input, descriptor.output, descriptor.criteria);
+      });
+    });
+  });
+}
