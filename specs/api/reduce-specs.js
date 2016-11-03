a66163053de8c9c04716e30e780ac1c0f069069b
weirdpattern <patricio@weirdpattern.com>
2016-11-02 23:19:17 -0500

    Make reduce more robust Make take validate the count and return a single value if no count is provided Sequence uses a clean copy of source every cycle. Add reduce specs
    changelog
    sqncr/
      lib/
        api/
          reduce.js
            - Calculate the initial value based on the type of sequence.source
          take.js
            - Validate count
            - Return single value when count is not provided
          to-array.js
            - Rename variables
        sequence.js
          - Store source
          - Use a clean copy of source every cycle.
        types.js
          - Reorder argument types in IterableObject.
      specs/
        Add reduce-specs.js

--- specs/api/reduce-specs.js
+++ specs/api/reduce-specs.js
@@ -13,6 +13,55 @@ describe('api', () => {
           value: GuardError,
         },
       },
+      'works without an input value with string sources': {
+        input: reduce('unexpected', (reduced, value) => {
+          reduced += value === 'e' ? 'a' : value;
+          return reduced;
+        }),
+        output: {
+          value: 'unaxpactad',
+        },
+      },
+      'works without an input value with array sources': {
+        criteria: 'toEqual',
+        input: [
+          reduce([1, 2, 3, 4], (reduced, value) => {
+            reduced.push(value + 1);
+            return reduced;
+          }),
+        ],
+        output: {
+          value: [2, 3, 4, 5],
+        },
+      },
+      'works without an input value with map sources': {
+        criteria: 'toEqual',
+        input: [
+          Array.from(
+            reduce(new Map([[1, 1], [2, 2], [3, 3], [4, 4]]), (reduced, value, key) => {
+              reduced.set(key, value + 1);
+              return reduced;
+            }).values()
+          ),
+        ],
+        output: {
+          value: [2, 3, 4, 5],
+        },
+      },
+      'works without an input value with set sources': {
+        criteria: 'toEqual',
+        input: [
+          Array.from(
+            reduce(new Set([1, 2, 3, 4]), (reduced, value) => {
+              reduced.add(value + 1);
+              return reduced;
+            }).values()
+          ),
+        ],
+        output: {
+          value: [2, 3, 4, 5],
+        },
+      },
     });
   });
 
